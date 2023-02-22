5ec7920ddca5baf6a14c1741d01d6ed5fb275351
Alexis <alexisgenieve@gmail.com>
2023-02-22 08:15:01 -0700

    Initial Commit

---
+++ Tech Social Media App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tech Social Media App
+//
+//  Created by Alexis Wright on 2/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
